6a221d33bb0af991109981e83e32026f3b75a0d7
Gary James Nave (Cognbuild) <59668562+gjnave@users.noreply.github.com>
2024-07-02 08:27:14 -0600

    Update wording.py

--- facefusion/wording.py
+++ facefusion/wording.py
@@ -127,7 +127,7 @@ WORDING : Dict[str, Any] =\
 		'stop_button': 'STOP',
 		'clear_button': 'CLEAR',
 		# about
-		'donate_button': 'DONATE',
+		'donate_button': 'Check out Jim Day & the Better Stories',
 		# benchmark
 		'benchmark_results_dataframe': 'BENCHMARK RESULTS',
 		# benchmark options
